c335d74baf2061126c6b75ac2c283b155f4b87af
Krishan-Shehan <shehankrishan6@gmail.com>
2020-09-25 00:31:03 +0530

    v2

--- app/src/main/java/com/example/yummy/MainActivity.java
+++ app/src/main/java/com/example/yummy/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.animation.ArgbEvaluator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
 
     ViewPager viewPager;
     Adapter adapter;
@@ -19,9 +26,22 @@ public class MainActivity extends AppCompatActivity {
     ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button22);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this,Ingredientst.class);
+               startActivity(intent);
+            }
+        });
+
+
+
+
 
         models = new ArrayList<>();
         models.add(new Model(R.drawable.egg_cress_club_sandwich_0, "Recipe1"));
