afc6b7c22fbdc9882180314eea6f72bfe0414841
Krishan-Shehan <shehankrishan6@gmail.com>
2020-09-24 16:39:16 +0530

    Home view updated

--- app/src/main/java/com/example/yummy/MainActivity.java
+++ app/src/main/java/com/example/yummy/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.yummy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
+    //Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        models = new ArrayList<>();
+        models.add(new Model(R.drawable.egg_cress_club_sandwich_0, "Recipe1"));
+        models.add(new Model(R.drawable.image__2, "Recipe2"));
+        models.add(new Model(R.drawable.image__5, "Recipe3"));
+        models.add(new Model(R.drawable.image__6, "Recipe4"));
+
+        adapter = new Adapter(models, this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+
+        //colors = colors_temp;
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 }
