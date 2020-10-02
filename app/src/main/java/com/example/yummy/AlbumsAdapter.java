e11b40a4a5ba85d3c42edc3ff898bd40b15ced10
Krishan-Shehan <shehankrishan6@gmail.com>
2020-10-02 15:34:47 +0530

    Recipe list

--- app/src/main/java/com/example/yummy/AlbumsAdapter.java
+++ app/src/main/java/com/example/yummy/AlbumsAdapter.java
@@ -1,15 +1,20 @@
 package com.example.yummy;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 //import com.bumptech.glide.Glide;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -24,12 +29,14 @@ public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHold
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title;
         public ImageView thumbnail, overflow;
+        public Button button;
 
         public MyViewHolder(View view) {
             super(view);
             title = (TextView) view.findViewById(R.id.title);
 //            count = (TextView) view.findViewById(R.id.count);
             thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            button = view.findViewById(R.id.fav);
 //            overflow = (ImageView) view.findViewById(R.id.overflow);
         }
     }
@@ -50,8 +57,25 @@ public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
-        Album album = albumList.get(position);
+        final Album album = albumList.get(position);
         holder.title.setText(album.getName());
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("fav",album.getName());
+
+                DatabaseReference def = FirebaseDatabase.getInstance().getReference("User").child("User1").child("fav");
+                try{
+
+                    def.child(album.getName()).setValue(album);
+//                    Log.d("ll", String.valueOf(album));
+
+                }catch (Exception e){
+
+                }
+
+            }
+        });
 //        holder.count.setText(album.getNumOfSongs() + " songs");
 
 //        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
--- app/src/main/java/com/example/yummy/RecipeList.java
+++ app/src/main/java/com/example/yummy/RecipeList.java
@@ -1,5 +1,6 @@
 package com.example.yummy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -8,8 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +28,8 @@ public class RecipeList extends AppCompatActivity {
     private RecyclerView recyclerView;
     private AlbumsAdapter adapter;
     private List<Album> albumList;
+    public List<String> ingredients = new ArrayList<>();
+    Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +38,77 @@ public class RecipeList extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         albumList = new ArrayList<>();
-        adapter = new AlbumsAdapter(this, albumList);
 
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        ingredients.add("True");
+        ingredients.add("Romance");
+
+
+        album = new Album();
+
+//        prepareAlbums();
+
+        int[] covers = new int[]{
+                R.drawable.album1,
+                R.drawable.album2,
+                R.drawable.album3,
+                R.drawable.album4,
+                R.drawable.album5,
+                R.drawable.album6,
+                R.drawable.album7,
+                R.drawable.album8,
+                R.drawable.album9,
+                R.drawable.album10,
+                R.drawable.album11};
+
+
+
+        adapter = new AlbumsAdapter(RecipeList.this, albumList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(RecipeList.this, 2);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
 
-        prepareAlbums();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Album");
+
+//        Query query1 = recipeRef.where("ingredients.tomato", "==", true).where("ingredients.pepper", "==", true).where("ingredients.cheese", "==", true);
 
 
+//        Query query = FirebaseDatabase.getInstance().getReference("Album")
+//                .orderByChild("test")
+//                .equalTo("kk");
+//                .startAt("Romance")
+//                .endAt("Romance"+"\uf8ff");
+
+        databaseReference.addListenerForSingleValueEvent(valueEventListener);
+
+//        query.addListenerForSingleValueEvent(valueEventListener);
 
     }
+
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                album = dataSnapshot.getValue( Album.class );
+                albumList.add(album);
+                Log.d("albumc",album.getName());
+                Log.d("albumc",String.valueOf(album.getThumbnail()));
+            }
+            Log.d("album", String.valueOf(albumList));
+            adapter.notifyDataSetChanged();
+
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+
+    };
 
     /**
      * Initializing collapsing toolbar
